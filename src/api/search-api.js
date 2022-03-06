b456f4f850cf1a75e50f273ecb81c13b9155c4d1
Joel Jeffrey <joeljeffrey.dev@gmail.com>
2022-03-05 18:25:48 -0800

    deleted unused file

--- src/api/search-api.js
+++
@@ -1,6 +0,0 @@
-// export function fetchUserRepos(username) {
-//   // fetch github user here
-//   return fetch(`https://api.github.com/users/${username}/repos`)
-//     .then((response) => response.json())
-//     .then((data) => data)
-// }
