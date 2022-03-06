48b047c402e4d6d88709a9de10c9e98469344fc7
Joel Jeffrey <joeljeffrey.dev@gmail.com>
2022-03-05 16:14:32 -0800

    removed unused class

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -3,7 +3,7 @@ import "./ListItem.css"
 function ListItem({ repoData }) {
   return (
     <li className="repo">
-      <span className="repo-name">{repoData.name}</span>
+      <span>{repoData.name}</span>
       <span className="repo-stars">&#9733; {repoData.stargazers_count}</span>
     </li>
   )
